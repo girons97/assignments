350e3f4f3bfa126980f582a1964491820a593649
George Irons <george.irons@live.co.uk>
2021-11-19 13:49:46 +0000

    Slightly change location of outcome to within loop

--- guess_the_number/guess_the_number.py
+++ guess_the_number/guess_the_number.py
@@ -1,4 +1,5 @@
 import random
+import sys
 secret_number = random.randint(0,100)
 
 def guess_a_num():
@@ -19,6 +20,7 @@ while True:
     guess = guess_a_num()
     if is_guess_a_pos_whole_num(guess):
         if guess == secret_number:
+            print('You win!')
             break
         elif guess > secret_number:
             print('You guessed too high, guess again!')
@@ -27,5 +29,5 @@ while True:
     else:
         print('You need to guess a positive, whole number between 0 and 100!')
 
-print('You win!')
+
 
